542f3d936f58b1474322e849980c2a990c0a2ce6
calmisential <xinyu_std@163.com>
2019-10-03 21:11:15 +0800

    Fix bug in GroupConv2D

--- models/resnext_block.py
+++ models/resnext_block.py
@@ -3,7 +3,8 @@ import tensorflow as tf
 
 class GroupConv2D(tf.keras.layers.Layer):
     def __init__(self,
-                 filters,
+                 input_channels,
+                 output_channels,
                  kernel_size,
                  strides=(1, 1),
                  padding='valid',
@@ -12,9 +13,14 @@ class GroupConv2D(tf.keras.layers.Layer):
                  use_bias=True,
                  groups=1):
         super(GroupConv2D, self).__init__()
+        if not input_channels % groups == 0:
+            raise ValueError("The value of input_channels must be divisible by the value of groups.")
+        if not output_channels % groups == 0:
+            raise ValueError("The value of output_channels must be divisible by the value of groups.")
         self.groups = groups
-        self.group_feature_map_num = filters // groups
-        self.conv = tf.keras.layers.Conv2D(filters=self.group_feature_map_num,
+        self.group_in_num = input_channels // groups
+        self.group_out_num = output_channels // groups
+        self.conv = tf.keras.layers.Conv2D(filters=self.group_out_num,
                                            kernel_size=kernel_size,
                                            strides=strides,
                                            padding=padding,
@@ -25,7 +31,7 @@ class GroupConv2D(tf.keras.layers.Layer):
 
     def call(self, inputs, **kwargs):
         for i in range(self.groups):
-            x_i = self.conv(inputs[:, :, :, i*self.group_feature_map_num: (i + 1) * self.group_feature_map_num])
+            x_i = self.conv(inputs[:, :, :, i*self.group_in_num: (i + 1) * self.group_in_num])
             self.feature_map_list.append(x_i)
         out = tf.concat(self.feature_map_list, axis=-1)
         return out
