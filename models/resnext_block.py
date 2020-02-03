f333d8584b90e6f794215d9779cdf07799d9a799
calmisential <xinyu_std@163.com>
2020-02-03 14:59:31 +0800

    Fix bug in GroupConv2D

--- models/resnext_block.py
+++ models/resnext_block.py
@@ -1,4 +1,6 @@
 import tensorflow as tf
+from tensorflow.keras import initializers, regularizers, constraints
+from tensorflow.keras import activations
 
 
 class GroupConv2D(tf.keras.layers.Layer):
@@ -10,28 +12,65 @@ class GroupConv2D(tf.keras.layers.Layer):
                  padding='valid',
                  data_format=None,
                  dilation_rate=(1, 1),
+                 activation=None,
+                 groups=1,
                  use_bias=True,
-                 groups=1):
+                 kernel_initializer='glorot_uniform',
+                 bias_initializer='zeros',
+                 kernel_regularizer=None,
+                 bias_regularizer=None,
+                 activity_regularizer=None,
+                 kernel_constraint=None,
+                 bias_constraint=None,
+                 **kwargs):
         super(GroupConv2D, self).__init__()
+
         if not input_channels % groups == 0:
             raise ValueError("The value of input_channels must be divisible by the value of groups.")
         if not output_channels % groups == 0:
             raise ValueError("The value of output_channels must be divisible by the value of groups.")
+
+        self.kernel_size = kernel_size
+        self.strides = strides
+        self.padding = padding
+        self.data_format = data_format
+        self.dilation_rate = dilation_rate
+        self.activation = activation
         self.groups = groups
+        self.use_bias = use_bias
+        self.kernel_initializer = kernel_initializer
+        self.bias_initializer = bias_initializer
+        self.kernel_regularizer = kernel_regularizer
+        self.bias_regularizer = bias_regularizer
+        self.activity_regularizer = activity_regularizer
+        self.kernel_constraint = kernel_constraint
+        self.bias_constraint = bias_constraint
+
         self.group_in_num = input_channels // groups
         self.group_out_num = output_channels // groups
-        self.conv = tf.keras.layers.Conv2D(filters=self.group_out_num,
-                                           kernel_size=kernel_size,
-                                           strides=strides,
-                                           padding=padding,
-                                           data_format=data_format,
-                                           dilation_rate=dilation_rate,
-                                           use_bias=use_bias)
+        self.conv_list = []
+        for i in range(self.groups):
+            self.conv_list.append(tf.keras.layers.Conv2D(filters=self.group_out_num,
+                                                         kernel_size=kernel_size,
+                                                         strides=strides,
+                                                         padding=padding,
+                                                         data_format=data_format,
+                                                         dilation_rate=dilation_rate,
+                                                         activation=activations.get(activation),
+                                                         use_bias=use_bias,
+                                                         kernel_initializer=initializers.get(kernel_initializer),
+                                                         bias_initializer=initializers.get(bias_initializer),
+                                                         kernel_regularizer=regularizers.get(kernel_regularizer),
+                                                         bias_regularizer=regularizers.get(bias_regularizer),
+                                                         activity_regularizer=regularizers.get(activity_regularizer),
+                                                         kernel_constraint=constraints.get(kernel_constraint),
+                                                         bias_constraint=constraints.get(bias_constraint),
+                                                         **kwargs))
 
     def call(self, inputs, **kwargs):
         feature_map_list = []
         for i in range(self.groups):
-            x_i = self.conv(inputs[:, :, :, i*self.group_in_num: (i + 1) * self.group_in_num])
+            x_i = self.conv_list[i](inputs[:, :, :, i*self.group_in_num: (i + 1) * self.group_in_num])
             feature_map_list.append(x_i)
         out = tf.concat(feature_map_list, axis=-1)
         return out
