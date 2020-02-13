7187da235ae68ed2fe0d21f48097c19c7f26f2f0
calmisential <xinyu_std@163.com>
2020-02-13 15:02:04 +0800

    Update required tensorflow version

--- train.py
+++ train.py
@@ -101,7 +101,7 @@ def process_features(features):
 
 if __name__ == '__main__':
     # GPU settings
-    gpus = tf.config.experimental.list_physical_devices('GPU')
+    gpus = tf.config.list_physical_devices("GPU")
     if gpus:
         for gpu in gpus:
             tf.config.experimental.set_memory_growth(gpu, True)
