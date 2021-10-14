6e7f801983ce02b11ae9e2cd51d99d1d7797f46e
calmisential <xinyu_std@163.com>
2021-10-14 14:14:33 +0800

    Change mode

--- show_model_list.py
+++ show_model_list.py
@@ -2,7 +2,7 @@ from models import get_model2idx_dict
 
 if __name__ == '__main__':
     print("Models included: ")
-    with open(file="models.txt", mode="a+", encoding="utf-8") as f:
+    with open(file="models.txt", mode="w", encoding="utf-8") as f:
         for model_name, i in get_model2idx_dict().items():
             print("MODEL: {}, INDEX: {}".format(model_name, i))
             f.write("{}: {}\n".format(model_name, i))
