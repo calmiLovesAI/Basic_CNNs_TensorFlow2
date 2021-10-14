60a400a646b1e39bb1ce49a868711801b0d6defa
calmisential <xinyu_std@163.com>
2021-10-14 13:46:19 +0800

    Generate models.txt

--- show_model_list.py
+++ show_model_list.py
@@ -2,5 +2,7 @@ from models import get_model2idx_dict
 
 if __name__ == '__main__':
     print("Models included: ")
-    for model_name, i in get_model2idx_dict().items():
-        print("MODEL: {}, INDEX: {}".format(model_name, i))
+    with open(file="models.txt", mode="a+", encoding="utf-8") as f:
+        for model_name, i in get_model2idx_dict().items():
+            print("MODEL: {}, INDEX: {}".format(model_name, i))
+            f.write("{}: {}\n".format(model_name, i))
